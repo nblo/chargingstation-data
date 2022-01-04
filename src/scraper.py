372dd47678f151e79388e41ec8f06f30219447ba
Nick Losacker <nicklosacker@gmail.com>
2022-01-04 18:24:25 +0100

    added main driver for scraper

---
+++ src/scraper.py
@@ -0,0 +1,53 @@
+import requests
+import datetime 
+import json 
+import time 
+import logging
+import typing 
+import os
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+BASE_URL_CHARGECLOUD = "https://new-poi.chargecloud.de"
+SCRAPING_INTERVAL = 10
+CITIES_UPDATED = ['koeln','dortmund','bonn','muenster','kiel',
+                  'chemnitz','krefeld','leverkusen','heidelberg',
+                  'solingen','ingolstadt','pforzheim','goettingen',
+                  'erlangen','tuebingen','bayreuth','bocholt','dormagen',
+                  'rastatt','hof','weinheim','bruchsal','nettetal','ansbach',
+                  'schwabach','ettlingen','crailsheim','deggendorf','forchheim',
+                  'bretten','buehl','zirndorf','roth','calw','herzogenaurach','wertheim',
+                  'kitzingen','lichtenfels']
+
+
+def scrape_cp_cities(cities: typing.List[str] = CITIES_UPDATED, 
+                     dir_save: str = "../data/scraped_data"): 
+    data_cities = {}
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")        
+
+    for city in cities: 
+        r = requests.get(BASE_URL_CHARGECLOUD + "/" + city)
+        try: 
+            data = json.loads(r.text)
+            data_cities[city] = data
+            logger.debug(f"Successfully scraped '{city}' at {now}.")
+        except: 
+            logger.error(f"Error occured while scraping '{city}' at {now}.")
+
+    fname = now + "_cp_data_cities.pkl"
+    path_save = os.path.join(dir_save, fname)
+    pd.to_pickle(data_cities, path_save)
+	
+
+def scraper(scraping_interval: typing.Union[int, float] = SCRAPING_INTERVAL): 
+    while True: 
+        
+        now = datetime.datetime.now().strftime("%Y/%m/%d_%H:%M:%S")
+        info_msg = f"Scraping cities: {now}"
+        logger.info(info_msg)
+        print(info_msg)
+        
+        scrape_cp_cities()
+        
+        time.sleep(scraping_interval*60 - 15)
