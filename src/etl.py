e09cd22906e5b0e04aad9ef4219c41eb4c8a3a29
Nick Losacker <nicklosacker@gmail.com>
2022-01-07 17:45:25 +0100

    deprecated etl with list structures

--- src/etl.py
+++ src/etl.py
@@ -2,110 +2,134 @@ import pandas as pd
 import logging 
 import configparser
 import psycopg2
+import typing
 
-from sql import (drop_table_queries,
-                 drop_constraints_queries, 
-                 create_table_queries,
-                 copy_table_queries,
-                 insert_table_queries,
+import logging.config
+from settings import LOGGING_CONFIG
+
+
+
+from sql import (DataTestCase,
+                 DataIngester,
+                 data_objects_staging, 
+                 data_objects,
                  SQL_CONSTRAINTS)
 
+
 logger = logging.getLogger(__name__)
+CONFIG_FILE = "../config.cfg"
 
 
-def drop_tables(cur, conn):
-    """Drop staging, fact and dimension tables from AWS Redshift
-
-    Args:
-        cur: AWS Redshift cursor object
-        conn: AWS Redshift connection object
-    """ 
-    for query in drop_table_queries:
-        logger.info(f"Executing query: {query}")
-        cur.execute(query)
+def _execute_query(query_fmt: str, mapping_fmt_query, cur, conn = None): 
+    
+    query = query_fmt.format(**mapping_fmt_query)
+    logger.info(f"Dropping table with query '{query:}'")
+    cur.execute(query)
+    
+    if conn: 
         conn.commit()
 
 
-def drop_constraints(cur, conn):
-    """Drop staging, fact and dimension tables from AWS Redshift
+def _perform_data_unit_test(data_test_cases: typing.List[DataTestCase], 
+                            cur,
+                            mapping_fmt_query: dict,
+                            table_name: str): 
+    
+    _mapping_fmt_query = {**mapping_fmt_query, "TABLE_NAME": table_name}
+    
+    for dtc in data_test_cases: 
+        logger.debug(f"Running data test case: '{dtc.name}'")
+        query = dtc.sql.format(**_mapping_fmt_query)
+        logger.debug(f"Running sql data test case: {dtc.sql}'")
+        records = pd.read_sql(con=cur, sql=query)
+            
+        logger.debug(f"Retrieved records of shape: '{records.shape}'.")
+        if records.shape[0] == 0: 
+            err_msg =  f"Data quality check failed. No result was returned."
+            logger.error(err_msg)
+            raise ValueError(err_msg)
+            
+        # first row of records contains the data unit test
+        data_unit_test = records[0]
+        
+        # either all or any values must evaluate to True 
+        _how = dtc.how
+        if _how(data_unit_test):
+            logger.info(f"Data quality check '{dtc.name}' passed.")
+        else: 
+            err_msg = f"Data quality check '{dtc.name}' failed. SQL statement '{dtc.sql}' evaluated to False."
+            logger.error(err_msg)
+            raise ValueError(err_msg)
+        
+    logger.info(f"All data quality checks passed for table '{table_name}'.")
 
-    Args:
-        cur: AWS Redshift cursor object
-        conn: AWS Redshift connection object
-    """ 
-    existing_constraints = set(pd.read_sql(con=conn, sql=SQL_CONSTRAINTS)["constraint_name"])
-    for query in drop_constraints_queries:
-        constraint_name = query.split("CONSTRAINT")[1].strip()
-        if constraint_name in existing_constraints: 
-            logger.info(f"Executing query: {query}")
-            cur.execute(query)
-            conn.commit()      
 
+def ingest_data(data_obj: DataIngester, cur, conn, mapping_fmt_queries: dict): 
+        
+        # dropping table 
+        if data_obj.drop_table: 
+            _execute_query(data_obj.drop_table, cur=cur, mapping_fmt_query=mapping_fmt_queries)
+        
+        # dropping constraints 
+        if data_obj.drop_constraints: 
+            existing_constraints = set(pd.read_sql(con=conn, sql=SQL_CONSTRAINTS)["constraint_name"])
+            constraint_name = data_obj.drop_constraints.split("CONSTRAINT")[1].strip()
+            
+            if constraint_name in existing_constraints: 
+                _execute_query(data_obj.drop_constraints, cur=cur, conn=conn, mapping_fmt_query=mapping_fmt_queries)
 
-def create_tables(cur, conn):
-    """Create staging, fact and dimension tables in AWS Redshift
+        # creating table 
+        if data_obj.create_table: 
+            _execute_query(data_obj.create_table, cur=cur, conn=conn, mapping_fmt_query=mapping_fmt_queries)
 
-    Args:
-        cur: AWS Redshift cursor object
-        conn: AWS Redshift connection object
-    """ 
-    for query in create_table_queries:
-        logger.info(f"Executing query: {query}")
-        cur.execute(query)
+        # populating table 
+        if data_obj.populate_table: 
+            _execute_query(data_obj.populate_table, cur=cur, conn=conn, mapping_fmt_query=mapping_fmt_queries)
+
         conn.commit()
         
+        # data quality checks
+        if data_obj.data_test_cases: 
+            _perform_data_unit_test(data_obj.data_test_cases, cur=cur, table_name=data_obj.table_name)
+        
 
-def load_staging_tables(cur, conn):
-    """Load song data and log data from files in S3 into staging tables in AWS Redshift 
-
-    Args:
-        cur: AWS Redshift cursor object
-        conn: AWS Redshift connection object
-    """    
-    for query in copy_table_queries:
-        logger.info(f"Executing query: {query}")
-        cur.execute(query)
-        conn.commit()
-
-
-def insert_tables(cur, conn):
-    """Insert data into fact and dimension tables from staging tables 
+def _create_data_model(cur, conn, data_objs: typing.List[DataIngester], mapping_fmt_queries: dict): 
     
-    Args:
-        cur: AWS Redshift cursor object
-        conn: AWS Redshift connection object
-    """    
-    for query in insert_table_queries:
-        logger.info(f"Executing query: {query}")
-        cur.execute(query)
-        conn.commit()
+    for dobj in data_objs: 
+        logger.info(f"Ingesting data into table '{dobj.table_name}'")
+        ingest_data(dobj, cur=cur, conn=conn, mapping_fmt_queries=mapping_fmt_queries)
 
 
-def main(config_file):
+def main(config_file, data_objects=data_objects):
+    logging.config.dictConfig(LOGGING_CONFIG)
+
     config = configparser.ConfigParser()
     config.read(config_file)
 
+    logger.info("Connecting to redshift database...")
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
+    logger.info("Successfully connected to redshift database...")
 
-    logger.info(f"Dropping tables.")
-    drop_tables(cur, conn)
+    mapping_fmt_queries = {"SCHEMA": config["CLUSTER"]["DB_SCHEMA"], 
+                           "ROLE_ARN": config["IAM_ROLE"]["ARN"], 
+                           "STATUS_DATA_CHARGING_POINT": config["S3"]["STATUS_DATA_CHARGING_POINT"], 
+                           "STATUS_DATA_CHARGING_CONNECTORS": config["S3"]["STATUS_DATA_CHARGING_CONNECTORS"],
+                           "MASTER_DATA_CHARGING_STATIONS":  config["S3"]["MASTER_DATA_CHARGING_STATIONS"], 
+                           "MASTER_DATA_CHARGING_POINTS":  config["S3"]["MASTER_DATA_CHARGING_POINTS"], 
+                           "MASTER_DATA_CONNECTORS":  config["S3"]["MASTER_DATA_CONNECTORS"], 
+                           "MASTER_DATA_CONNECTORS":  config["S3"]["MASTER_DATA_CONNECTORS"]
+                           }
     
-    logger.info(f"Dropping constraints.")
-    drop_constraints(cur, conn)
+    logger.info(f"Ingesting data into staging tables.")
+    _create_data_model(cur=cur, conn=conn, data_objs=data_objects_staging, mapping_fmt_queries=mapping_fmt_queries)
     
-    logger.info(f"Creating tables.")
-    create_tables(cur, conn)
-    
-    logger.info(f"Loading staging tables.")
-    load_staging_tables(cur, conn) 
-    
-    
-    logger.info(f"Ingesting data from staging tables.")
-    insert_tables(cur, conn)
+    logger.info(f"Ingesting data into data model.")
+    _create_data_model(cur=cur, conn=conn, data_objs=data_objects, mapping_fmt_queries=mapping_fmt_queries)
     
     conn.close()
     
 
 if __name__ == '__main__': 
-    main()
+    logging.config.dictConfig(LOGGING_CONFIG)
+    main(config_file=CONFIG_FILE)
